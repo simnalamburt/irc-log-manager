5b0051538e4ac152f9e27c4d8e92f92068c1fc1d
Hyeon Kim <simnalamburt@gmail.com>
2019-05-18 16:52:55 +0900

    Check IRC log files in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,47 @@
+use failure::Fallible;
 use memmap::MmapOptions;
+use rayon::prelude::*;
 use regex::Regex;
 use std::env;
-use std::error::Error;
-use std::fs::{File, read_dir};
+use std::fs::{read_dir, File};
 
-fn main() -> Result<(), Box<Error>> {
+fn main() -> Fallible<()> {
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\t$")?;
 
     let home_dir = env::var("HOME")?;
     let log_dir = format!(r"{}/.weechat/logs/", home_dir);
-    for entry in read_dir(log_dir)? {
-        let entry = entry?;
-        let file = File::open(entry.path())?;
-        let mmap = unsafe { MmapOptions::new().map(&file)? };
+    let entries: Vec<_> = read_dir(log_dir)?.collect();
 
-        // Create index
-        for i in 0..mmap.len() {
-            if mmap[i] != b'\n' { continue }
+    entries
+        .into_par_iter()
+        .map(|entry| -> Fallible<_> {
+            let entry = entry?;
+            let file = File::open(entry.path())?;
+            let mmap = unsafe { MmapOptions::new().map(&file)? };
 
-            let end = i + 21;
-            if end > mmap.len() { continue }
+            // Create index
+            for i in 0..mmap.len() {
+                if mmap[i] != b'\n' {
+                    continue;
+                }
 
-            let words = std::str::from_utf8(&mmap[i..end])?;
-            if !re.is_match(words) { continue }
+                let end = i + 21;
+                if end > mmap.len() {
+                    continue;
+                }
 
-            println!("File {:?} has issue with {}th byte", entry.file_name(), i);
-            break
-        }
-    }
+                let words = std::str::from_utf8(&mmap[i..end])?;
+                if !re.is_match(words) {
+                    continue;
+                }
+
+                println!("File {:?} has issue with {}th byte", entry.file_name(), i);
+                break;
+            }
+
+            Ok(())
+        })
+        .collect::<Result<_, _>>()?;
 
     Ok(())
 }
