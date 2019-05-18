0b2b7bc12665247461e6ba53a4b7a7f0db51f840
Hyeon Kim <simnalamburt@gmail.com>
2019-05-18 18:12:00 +0900

    Parse ARGV with 'clap'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{clap_app, crate_version, App, AppSettings};
 use failure::Fallible;
 use memmap::MmapOptions;
 use rayon::prelude::*;
@@ -6,6 +7,24 @@ use std::env;
 use std::fs::{read_dir, File};
 
 fn main() -> Fallible<()> {
+    let matches = clap_app!(
+        @app(App::new("irc-log-manager"))
+        (version: crate_version!())
+        (about: "Manage IRC logs of weechat")
+        (@subcommand check => (about: "check if IRC logs are intact"))
+        (@subcommand rank => (about: "sort IRC channels in order of recent activity"))
+    )
+    .setting(AppSettings::ArgRequiredElseHelp)
+    .get_matches();
+
+    match matches.subcommand_name() {
+        Some("check") => check(),
+        Some("rank") => unimplemented!(),
+        _ => unreachable!(),
+    }
+}
+
+fn check() -> Fallible<()> {
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\t$")?;
 
     let home_dir = env::var("HOME")?;
@@ -18,8 +37,6 @@ fn main() -> Fallible<()> {
             let entry = entry?;
             let file = File::open(entry.path())?;
             let mmap = unsafe { MmapOptions::new().map(&file)? };
-
-            // Create index
             for i in 0..mmap.len() {
                 if mmap[i] != b'\n' {
                     continue;
@@ -38,10 +55,7 @@ fn main() -> Fallible<()> {
                 println!("File {:?} has issue with {}th byte", entry.file_name(), i);
                 break;
             }
-
             Ok(())
         })
-        .collect::<Result<_, _>>()?;
-
-    Ok(())
+        .collect()
 }
